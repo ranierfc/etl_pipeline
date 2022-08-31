e9ce752567fa538b4a10cc30d26da6161310dd94
Ranier Cordeiro <raniercordeiro.root@gmail.com>
2022-08-31 20:23:15 -0300

    sql create table

--- lhama.sql
+++ lhama.sql
@@ -1,9 +1,9 @@
 create table if not exists artist(
 	id 			    integer primary key autoincrement,
-	first_name	    text    not null,
+	first_name	    text,
 	second_name     text,
 	surname		    text,
-	artist_id	    integer not null unique,
-	link			text	not null unique,
-	extraction_date	integer not null
+	artist_id	    integer,
+	link			text,
+	extraction_date	integer
 );
